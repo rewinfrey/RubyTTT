210975b1dfb2185e9a24379057d9c8b76f923be7
Rick Winfrey <rick.winfrey@gmail.com>
2013-01-28 18:55:02 -0600

    deletes unnecessary files as a result of removing code duplication in limelight interface

--- limelight/four_by_four/players/four_by_four.rb
+++
@@ -1,8 +0,0 @@
-on_scene_opened do
-  production.limelight_game = GamePlayscript.new(:game_id => production.game_id, :context => scene, :production => production)
-  if production.context.finished?(production.game_id)
-    production.limelight_game.display_results
-  else
-    production.limelight_game.process_ai_move
-  end
-end
